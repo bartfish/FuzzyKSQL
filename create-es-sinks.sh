420c1fee61a5a4515dd7b82ce8cea5085c1927a8
Bartlomiej <bartlomiejrybaa@gmail.com>
2021-01-06 01:28:29 +0100

    updated files

--- create-es-sinks.sh
+++ create-es-sinks.sh
@@ -24,10 +24,19 @@ curl -X "POST" "http://localhost:8083/connectors/" \
   "config": {
     "connector.class": "io.confluent.connect.elasticsearch.ElasticsearchSinkConnector",
     "connection.url": "http://elasticsearch:9200",
-    "type.name": "ksql-workshop",
-    "tasks.max": "1",
+    "type.name": "workshop-u",
     "schema.ignore": "true",
     "topics": "workshop-users",
     "write.method": "upsert"
   }
 }'
+
+CREATE SINK CONNECTOR SINK_ELASTIC_TEST_01 WITH (
+  'connector.class' = 'io.confluent.connect.elasticsearch.ElasticsearchSinkConnector',
+  'connection.url'  = 'http://elasticsearch:9200',
+  'key.converter'   = 'org.apache.kafka.connect.storage.StringConverter',
+  'type.name'       = '_doc',
+  'topics'          = 'workshop-u',
+  'key.ignore'      = 'true',
+  'schema.ignore'   = 'false'
+);
