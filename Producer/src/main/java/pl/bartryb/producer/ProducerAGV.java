279bb14ad74722d0148b329519a7b11db236c34b
Bartłomiej Ryba <bartlomiejrybaa@gmail.com>
2021-03-02 11:04:47 +0100

    second generator added

--- Producer/src/main/java/pl/bartryb/producer/ProducerAGV.java
+++ Producer/src/main/java/pl/bartryb/producer/ProducerAGV.java
@@ -6,6 +6,7 @@ import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.LongSerializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import pl.bartryb.producer.models.AutonomousGuidedVehicle;
+import pl.bartryb.producer.models.AutonomousGuidedVehicleOther;
 
 import java.util.*;
 import java.util.concurrent.ExecutionException;
@@ -14,6 +15,83 @@ import java.util.concurrent.ExecutionException;
 public class ProducerAGV {
 
     private static List<String> machineStates = Arrays.asList(new String[] { "STANDBY", "WORKING", "BROKEN", "OFFLINE"});
+    private static List<String> weightUnits = Arrays.asList(new String[]
+            { "kg", "kilogram", "KG", "kilogr", "gram", "ml", "l"});
+
+    private static void defineStream1(KafkaProducer<String, AutonomousGuidedVehicle> producer, String topic, String key) {
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run()  {
+
+                Random rand = new Random();
+                // task to run goes here
+                //   System.out.println("Hello !!!");
+                AutonomousGuidedVehicle agv = new AutonomousGuidedVehicle();
+                agv.traction = rand.nextDouble();; // 0 - 100
+                agv.latitude = rand.nextDouble(); // 50.0000001
+                agv.longtitute = rand.nextDouble(); // 50.0000001
+                agv.machineState = machineStates.get(rand.nextInt(3)); // STANDBY, WORKING, BROKEN, LOST_CONNECTION, LOST
+                agv.humidity = rand.nextDouble(); // 0 - 10 (0 means dry, 10 means a lot of water)
+                agv.batteryPercentageLeft = rand.nextDouble(); // 0 - 100 (battery lasting percentage)
+
+                ProducerRecord<String, AutonomousGuidedVehicle> record
+                        = new ProducerRecord<String, AutonomousGuidedVehicle>(topic, key, agv);
+                try {
+                    producer.send(record).get();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        Timer timer = new Timer();
+        long delay = 0;
+        long intevalPeriod = 1 * 1000;
+
+        // schedules the task to be run in an interval
+        timer.scheduleAtFixedRate(task, delay,
+                intevalPeriod);
+    }
+
+    private static void defineStream2(KafkaProducer<String, AutonomousGuidedVehicleOther> producer, String topic, String key) {
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run()  {
+
+                Random rand = new Random();
+                // task to run goes here
+                //   System.out.println("Hello !!!");
+                AutonomousGuidedVehicleOther agv = new AutonomousGuidedVehicleOther();
+                agv.traction = rand.nextDouble();; // 0 - 100
+                agv.latitude = rand.nextDouble(); // 50.0000001
+                agv.longtitute = rand.nextDouble(); // 50.0000001
+                agv.machineState = machineStates.get(rand.nextInt(3)); // STANDBY, WORKING, BROKEN, LOST_CONNECTION, LOST
+                agv.humidity = rand.nextDouble(); // 0 - 10 (0 means dry, 10 means a lot of water)
+                agv.batteryPercentageLeft = rand.nextDouble(); // 0 - 100 (battery lasting percentage)
+                agv.weightValue =  rand.nextDouble();
+                agv.weightUnit = weightUnits.get(rand.nextInt(weightUnits.size() - 1 ));
+
+                ProducerRecord<String, AutonomousGuidedVehicleOther> record
+                        = new ProducerRecord<String, AutonomousGuidedVehicleOther>(topic, key, agv);
+                try {
+                    producer.send(record).get();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        Timer timer = new Timer();
+        long delay = 0;
+        long intevalPeriod = 1 * 1500;
+
+        // schedules the task to be run in an interval
+        timer.scheduleAtFixedRate(task, delay,
+                intevalPeriod);
+    }
+
 
     public static void KSProducer() throws ExecutionException, InterruptedException {
         try {
@@ -24,43 +102,17 @@ public class ProducerAGV {
             props.put("schema.registry.url", "http://localhost:8081");
 
             KafkaProducer<String, AutonomousGuidedVehicle> producer = new KafkaProducer<>(props);
+            KafkaProducer<String, AutonomousGuidedVehicleOther> producerOther = new KafkaProducer<>(props);
 
-            String topic = "AGV_1";
-            String key = "testkey";
+            String topic1 = "AGV_1";
+            String key1 = "testkey1";
 
-            TimerTask task = new TimerTask() {
-                @Override
-                public void run()  {
+            String topic2 = "AGV_2";
+            String key2 = "testkey2";
 
-                    Random rand = new Random();
-                  // task to run goes here
-                //   System.out.println("Hello !!!");
-                    AutonomousGuidedVehicle agv = new AutonomousGuidedVehicle();
-                    agv.traction = rand.nextDouble();; // 0 - 100
-                    agv.latitude = rand.nextDouble(); // 50.0000001
-                    agv.longtitute = rand.nextDouble(); // 50.0000001
-                    agv.machineState = machineStates.get(rand.nextInt(3)); // STANDBY, WORKING, BROKEN, LOST_CONNECTION, LOST
-                    agv.humidity = rand.nextDouble(); // 0 - 10 (0 means dry, 10 means a lot of water)
-                    agv.batteryPercentageLeft = rand.nextDouble(); // 0 - 100 (battery lasting percentage)
+            defineStream1(producer, topic1, key1);
+            defineStream2(producerOther, topic2, key2);
 
-                    ProducerRecord<String, AutonomousGuidedVehicle> record
-                            = new ProducerRecord<String, AutonomousGuidedVehicle>(topic, key, agv);
-                    try {
-                        producer.send(record).get();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    } catch (ExecutionException e) {
-                        e.printStackTrace();
-                    }
-                }
-              };
-              Timer timer = new Timer();
-              long delay = 0;
-              long intevalPeriod = 1 * 1000; 
-              
-              // schedules the task to be run in an interval 
-              timer.scheduleAtFixedRate(task, delay,
-                                          intevalPeriod);
                                           
         } catch (Exception e) {
             System.out.println("Message: " + e.getMessage() + " StackTrace: " + e.getStackTrace().toString());
