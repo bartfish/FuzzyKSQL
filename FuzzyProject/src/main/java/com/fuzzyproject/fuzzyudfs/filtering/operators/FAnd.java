2e3ee4022bcc5265ce1cea0e73837564a138b655
Bartłomiej Ryba <bartlomiejrybaa@gmail.com>
2021-04-01 00:23:36 +0200

    fuzzy and probably implemented

--- FuzzyProject/src/main/java/com/fuzzyproject/fuzzyudfs/filtering/operators/FAnd.java
+++ FuzzyProject/src/main/java/com/fuzzyproject/fuzzyudfs/filtering/operators/FAnd.java
@@ -1,27 +1,33 @@
-//package com.fuzzyproject.fuzzyudfs.filtering.operators;
-//
-//import com.fuzzyproject.functions.FunctionIdentifier;
-//import com.fuzzyproject.functions.mixed.LinguisticDef;
-//import io.confluent.ksql.function.udf.Udf;
-//import io.confluent.ksql.function.udf.UdfParameter;
-//import io.confluent.ksql.function.udf.UdfDescription;
-//import org.jetbrains.annotations.Nullable;
-//
-//@UdfDescription(name = "FAnd", description = "check if value fits specific linguistic one")
-//public class FAnd {
-//
-//    @Udf(description = "Overriden AND operator, max of ANDs is 6 and at least 2 arguments are required")
-//    public static Boolean FAnd(
-//            @UdfParameter(value = "condition1", description = "Condition1") final String condition1,
-//            @UdfParameter(value = "condition2", description = "Condition2") final String condition2
-//    ) {
-//
+package com.fuzzyproject.fuzzyudfs.filtering.operators;
+
+import com.fuzzyproject.functions.FunctionIdentifier;
+import com.fuzzyproject.functions.mixed.LinguisticDef;
+import io.confluent.ksql.function.udf.Udf;
+import io.confluent.ksql.function.udf.UdfParameter;
+import io.confluent.ksql.function.udf.UdfDescription;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+@UdfDescription(name = "FUZZY_AND", description = "check if value fits specific linguistic one")
+public class FAnd {
+
+    @Udf(description = "Overriden AND operator, max of ANDs is 6 and at least 2 arguments are required")
+    public static Boolean FAnd(@UdfParameter List<Boolean> elements) {
+
+
+        for(int i = 0; i < elements.size(); i++) {
+            if (!elements.get(i)) {
+                return false;
+            }
+        }
+
 //        System.out.println(FunctionIdentifier.IdentifyFunction(condition1));
 //        System.out.println(FunctionIdentifier.IdentifyFunction(condition2));
-//
-////        String value = LinguisticDef.defineLinguisticRanges(expression, searchedValue);
-////        return value == linguisticValue;
-//        return true;
-//    }
-//
-//}
+
+//        String value = LinguisticDef.defineLinguisticRanges(expression, searchedValue);
+//        return value == linguisticValue;
+        return true;
+    }
+
+}
