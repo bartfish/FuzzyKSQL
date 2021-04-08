828880c912f90bd25a2a5bb9fab5d5f4f08347e0
Bartłomiej Ryba <bartlomiejrybaa@gmail.com>
2021-04-08 11:52:10 +0200

    updated fuzzy_and - various number of arguments allowed

--- FuzzyProject/src/main/java/com/fuzzyproject/fuzzyudfs/filtering/is/IsLing.java
+++ FuzzyProject/src/main/java/com/fuzzyproject/fuzzyudfs/filtering/is/IsLing.java
@@ -5,11 +5,11 @@ import io.confluent.ksql.function.udf.Udf;
 import io.confluent.ksql.function.udf.UdfParameter;
 import io.confluent.ksql.function.udf.UdfDescription;
 
-@UdfDescription(name = "VerifyIs", description = "check if value fits specific linguistic one")
+@UdfDescription(name = "VERIFYIS", description = "check if value fits specific linguistic one")
 public class IsLing {
 
     @Udf(description = "Check whether value fits linguistic match defined in arguments")
-    public Boolean Is(
+    public static Boolean VerifyIs(
             @UdfParameter(value = "expression", description = "exemplary expression: TRIA;LEFT;MIDDLE;RIGHT") final String expression,
             @UdfParameter(value = "seachedValue", description = "searchedValue") final Double searchedValue,
             @UdfParameter(value = "linguisticValue", description = "linguisticValue") final String linguisticValue,
